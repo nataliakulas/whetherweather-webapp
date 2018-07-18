307e4b6eb0c474e0c8df4b6b6a530a88ab44f4a3
Natalia Kulas <nataliakulas@gmail.com>
2018-07-18 23:34:28 +0200

    Display window loader added

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -1,71 +1,48 @@
 import React from 'react';
-import { Row, Col } from 'react-grid-system';
+import {Row, Col} from 'react-grid-system';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import theme from '../shared/theme';
 
-import { SquareButton, Button } from './Styles';
+import {SquareButton, Button} from './Styles';
 
 const ListItemWrapper = styled.div`
     width: 100%;
     margin: 10px auto;
-    background-color: ${({ active }) => active ? theme.light : theme.primaryBlue };
+    background-color: ${({active}) => active ? theme.light : theme.primaryBlue };
     border: 1px solid;
-    border-color: ${({ active }) => active ? theme.light : theme.secondaryBlue };
+    border-color: ${({active}) => active ? theme.light : theme.secondaryBlue };
     border-radius: 2px;
     padding: 0 10px;
 `;
 
-class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
+const ListItem = ({latitude, longitude, position, toggleFav, showWeather, active, fav}) => {
 
-  componentDidMount() {
-    this.setState({ loading: false });
-  }
-
-  render() {
-    const { latitude, longitude, position, toggleFav, showWeather, active, fav } = this.props;
-    const { loading } = this.state;
-
-    if (loading) {
-      return (
-        <div>
-          Loading
-        </div>
-      );
-    }
-
-    return (
-      <ListItemWrapper active={active}>
-        <Row style={{ width: '100%', alignItems: 'center', justifyContent: 'space-between' }} nogutter>
-          <Col xs={2} xl={2}>
-            <SquareButton onClick={toggleFav} disabled={!latitude || !longitude} type="button" active={fav} style={{ marginLeft: 0 }} />
-          </Col>
-          <Col xs={7} xl={8}>
-            <p style={{ fontSize: 14, margin: '0 10px' }}>
-              {position[0]}
-              {position[1] === 'location' ? ' ' : ', '}
-              {position[1]}
-              <br />
-              {latitude}
-              ,&nbsp;
-              {longitude}
-            </p>
-          </Col>
-          <Col xs={3} xl={2}>
-            <Button onClick={showWeather} type='button' style={{fontSize: 12, textTransform:'uppercase'}}>
-              Check
-            </Button>
-          </Col>
-        </Row>
-      </ListItemWrapper>
-    );
-  }
+  return (
+    <ListItemWrapper active={active}>
+      <Row style={{width: '100%', alignItems: 'center', justifyContent: 'space-between'}} nogutter>
+        <Col xs={2}>
+          <SquareButton onClick={toggleFav} disabled={!latitude || !longitude} type="button" active={fav} style={{marginLeft: 0}} />
+        </Col>
+        <Col xs={7}>
+          <p style={{fontSize: 14, margin: '0 10px 0 5px'}}>
+            {position[0]}
+            {position[1] === 'location' ? ' ' : ', '}
+            {position[1]}
+            <br />
+            {latitude}
+            ,&nbsp;
+            {longitude}
+          </p>
+        </Col>
+        <Col xs={3}>
+          <Button onClick={showWeather} type='button' style={{fontSize: 12, textTransform: 'uppercase'}}>
+            Check
+          </Button>
+        </Col>
+      </Row>
+    </ListItemWrapper>
+  );
 }
 
 ListItem.defaultProps = {
